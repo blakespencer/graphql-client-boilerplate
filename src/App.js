40f28df8dfd929d71a41b23affd9d9cf824a142c
Blake Spencer <blake.spencer1@gmail.com>
2018-10-09 15:11:01 -0400

    [feature] set up client connection with server 'localhost:4000/graphql'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset';
+import gql from 'graphql-tag';
+
+const httpLink = new HttpLink({ uri: 'http://localhost:4000/graphql' });
+
+// We need to specify cache
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+console.log(client);
+
+client
+  .query({
+    query: gql`
+      {
+        users {
+          firstName
+          lastName
+        }
+      }
+    `,
+  })
+  .then(result => console.log(result));
 
 class App extends Component {
   render() {
